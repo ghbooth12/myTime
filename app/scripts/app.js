8de9b32b6a53e821860b8b200bb260c04ad6408d
Gahee Heo <ghbooth12@gmail.com>
2016-07-12 14:40:18 -0500

    us1-angularfire-configuration: completed

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+  angular
+    .module('myTime', ['ui.router', 'firebase'])
+    .config(config);
+
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('main', {
+        url: '/',
+        controller: 'MainCtrl as main',
+        templateUrl: '/templates/main.html'
+      });
+  }
+})();
