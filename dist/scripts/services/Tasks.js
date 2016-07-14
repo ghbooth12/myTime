5d05a3184cc8250bee471d1cedf5609e4759e88a
Gahee Heo <ghbooth12@gmail.com>
2016-07-13 22:15:39 -0500

    us7-record-completed-tasks: completed

---
+++ dist/scripts/services/Tasks.js
@@ -0,0 +1,22 @@
+(function() {
+  angular
+    .module('myTime')
+    .factory('Tasks', ['$firebaseArray', Tasks]);
+
+  function Tasks($firebaseArray) {
+    var ref = new Firebase('https://my-time-8059d.firebaseio.com');
+    var tasks = $firebaseArray(ref.child('tasks'));
+
+    var tasksObj = {
+      all: tasks,
+      add: function(taskItem) {
+        if (taskItem) {
+          tasks.$add({item: taskItem});
+          this.taskItem = null;
+        }
+      }
+    };
+
+    return tasksObj;
+  }
+})();
